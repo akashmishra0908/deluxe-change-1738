27e26d2d8094a3222fe5a3505da1557b585198b4
Anburaj <anburaj2203@gmail.com>
2023-09-28 14:27:53 +0530

    created signin and signup pages

--- client/src/components/AllRoutes.jsx
+++ client/src/components/AllRoutes.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
+import { Signin } from '../pages/Signin'
+import { Signup } from '../pages/SignUp'
 
 const AllRoutes = () => {
   return (
     <Routes>
         <Route path="/" element={<HomePage/>}/>
+        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/signup" element={<Signup/>}/>
         <Route path="*" element={<h3>Page Not Found</h3>}/>
     </Routes>
   )
