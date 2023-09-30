989d4e73b7dd8dfac0de5ade185b3447adfeab4c
unknown <anuragkushwah134@gmail.com>
2023-09-30 21:27:56 +0530

    ok;

--- server/routes/course.route.js
+++ server/routes/course.route.js
@@ -58,7 +58,7 @@ router.patch("/update/:id",async(req,res)=>{
     const course=await CourseModel.findById(id)
     try {
         if(useremail==course.email){
-            const courses=await CourseModel.findByIdAndUpdate(id,req.body)
+            const course=await CourseModel.findByIdAndUpdate(id,req.body)
             const data=await CourseModel.findById(id)
            return res.status(200).send({"Course":data})  
         }
