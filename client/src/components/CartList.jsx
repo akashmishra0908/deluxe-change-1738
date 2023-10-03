d184275e159c81d6146dbbaea3b1d979637aeef2
akash mishra <112822976+akashmishra0908@users.noreply.github.com>
2023-10-03 13:26:14 +0530

    Update CartList.jsx

--- client/src/components/CartList.jsx
+++ client/src/components/CartList.jsx
@@ -17,7 +17,7 @@ import {
   useDisclosure,
 } from "@chakra-ui/react";
 import { StarIcon } from "@chakra-ui/icons";
-import { deleteCart, getcart } from "../redux/cart/action";
+import { getcart } from "../redux/cart/action";
 import { useDispatch } from "react-redux";
 import { MdLocalOffer } from "react-icons/md";
 export const CartList = ({
@@ -34,11 +34,6 @@ export const CartList = ({
   const dispatch = useDispatch();
   const toast = useToast();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const handleDelete = (_id) => {
-    dispatch(deleteCart(_id)).then((res) => {
-      dispatch(getcart);
-    });
-  };
 
   return (
     <Box pt={"16px"}>
@@ -100,9 +95,6 @@ export const CartList = ({
             display="block"
             _hover={{
               backgroundColor: "none",
-            }}
-            onClick={() => {
-              handleDelete(_id);
             }}
           >
             <Text
