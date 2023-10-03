2027b8143eff41ecd12e265068b04b28ff1f4acb
akashmishra0908 <112822976+akashmishra0908@users.noreply.github.com>
2023-10-03 20:43:04 +0530

    last commit

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getcart } from "../redux/cart/action";
 import { Box, Text, Input, Button, Flex, Stack } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import { CartList } from "../components/CartList";
+import axios  from "axios";
+
 
 const Cart = () => {
   const dispatch = useDispatch();
@@ -12,6 +14,7 @@ const Cart = () => {
   const [couponCode, setCouponCode] = useState("");
   const [discount, setDiscount] = useState(0);
   const [state, setState] = useState(false);
+  const navigate=useNavigate();
 
   const handleApplyCoupon = () => {
     if (couponCode === "KEEPLEARNING20") {
@@ -30,7 +33,55 @@ const Cart = () => {
   cart.forEach((el) => {
     totalPrice += el.price;
   });
+  const handleOprnrazorPay =(data)=>{
+    const options = {
+        key: 'rzp_test_nJzVZbVKoUGrDj',
+        amount: Number(data.amount),
+        currency: data.currency,
+        order_id: data.id,
+        name: 'eTutorHUb',//
+        description: 'MY WEBSITE',//
+        handler: function (response) {
+            console.log(response, "56")
+            axios.post('https://flipkart-clone-o5h3.onrender.com/verify', { response: response })
+                .then(res => {
+                    console.log(res, "37")
+                    // your orders
+                    navigate('/');
+                    // setTimeout(()=>{
+                    //     dispatch(afterPayment());
+                    // },1000)
+                    
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+                
+        }
 
+    }
+    const rzp = new window.Razorpay(options)
+    rzp.open()
+}
+
+const buyNow = async (amount) => {
+    // let response = await payUsingPaytm({ amount: 100, email: 'aman1722@gmail.com'});
+    // var information = {
+    //     action: 'https://securegw.paytm.in/order/process',
+    //     params: response    
+    // }
+    // post(information);
+    const _data={amount:amount}
+    axios.post(`https://flipkart-clone-o5h3.onrender.com/orders`,_data)
+    .then(res=>{
+        console.log(res.data);
+        handleOprnrazorPay(res.data.data);
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+
+}
   const discountedPrice = totalPrice - totalPrice * discount;
 
   return (
@@ -80,8 +131,9 @@ const Cart = () => {
               ₹{discountedPrice.toFixed(2)}{" "}
             </Text>
           </Stack>
-          <Link to="/payment">
+          
             <Button
+            onClick={()=>{buyNow(totalPrice)}}
               bg="#9575CD"
               color="white"
               _hover={{
@@ -93,7 +145,7 @@ const Cart = () => {
             >
               Checkout
             </Button>
-          </Link>
+         
           <hr style={{ border: "1px solid #E0E0E0", marginTop: "13px" }} />
           <Text
             fontSize={["14px", "16px"]}
